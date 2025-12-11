8c76e3bf038c30af356b3f9e55108ce57f1b2efa
ZetsuBedrech <jr17000@live.fr>
2025-12-11 22:39:32 +0100

    nouveau jeu

---
+++ js/theme_bataille.js
@@ -0,0 +1,40 @@
+function Start() {
+    const alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    const themeUpper = [
+    "ANIMAUX TERRESTRES",
+    "FRUITS",
+    "LEGUMES",
+    "PAYS",
+    "CAPITALES",
+    "CORPS",
+    "MEUBLES",
+    "COULEURS",
+    "METIERS",
+    "PRENOMS",
+    "NOURRITURE",
+    "BOISSONS",
+    "OBJETS",
+    "OUTILS",
+    "JEUX VIDEO",
+    "FILMS / SERIES",
+    "ANIMAUX MARINS",
+    "INSECTES"
+];
+
+    const letterDiv = document.getElementById("letter");
+    const themeDiv = document.getElementById("theme");
+
+    // Fonction pour piocher un ElEment au hasard
+    function pickRandom(arr) {
+        const index = Math.floor(Math.random() * arr.length);
+        return arr[index];
+    }
+
+    const letter = pickRandom(alphabetUpper);
+    const theme = pickRandom(themeUpper);
+
+    letterDiv.textContent = letter;
+    themeDiv.textContent = theme;
+}
+
+Start();
